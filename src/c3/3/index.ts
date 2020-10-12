b3e0b7cc4b56dac5fadd1b5e96e552604f1847d4
zjhcn <332124614@qq.com>
2020-10-12 17:44:39 +0800

    feat(c2):
    - 顺时针绘制圆

---
+++ src/c3/3/index.ts
@@ -0,0 +1,10 @@
+import { dom } from "@/utils/dom";
+
+const cnv = dom("canvas") as HTMLCanvasElement;
+const cxt = cnv.getContext("2d") as CanvasRenderingContext2D;
+
+cxt.beginPath();
+cxt.arc(70, 70, 50, 0, -90 * Math.PI / 180, false);
+cxt.closePath();
+cxt.strokeStyle = "HotPink";
+cxt.stroke();
