f3cc40f0da8696d9853daaa82779d32f05306121
zjhcn <332124614@qq.com>
2020-10-12 17:04:22 +0800

    feat(c2):
    - 画出正方形

---
+++ src/c2/4/index.ts
@@ -0,0 +1,11 @@
+import { dom } from "@/utils/dom";
+
+const cnv = dom("canvas") as HTMLCanvasElement;
+const cxt = cnv.getContext("2d") as CanvasRenderingContext2D;
+
+cxt.moveTo(50, 100);
+cxt.lineTo(50, 50);
+cxt.lineTo(150, 50);
+cxt.lineTo(150, 100);
+cxt.lineTo(50, 100);
+cxt.stroke();;
