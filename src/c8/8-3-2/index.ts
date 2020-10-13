231533acfbb0b997412995144f8dc51a745b35d0
zjhcn <332124614@qq.com>
2020-10-13 16:44:08 +0800

    feat(c8 - 像素操作):
    - 黑白效果 算法使用加权平均值

---
+++ src/c8/8-3-2/index.ts
@@ -0,0 +1,28 @@
+import { dom } from "@/utils/dom";
+
+const cnv = dom("canvas") as HTMLCanvasElement;
+const cxt = cnv.getContext("2d") as CanvasRenderingContext2D;
+
+; (async function () {
+  // 黑白效果，也叫灰度图（average），是指将彩色图片转换成黑白图片。
+  // 实现算法是：首先取红、绿、蓝三个通道的平均值，
+  // 也就是(data[i + 0] + data[i + 1] +data[i + 2])/3。
+  // 然后data[i + 0]、data[i + 1]和data[i + 2]全部保存为这个平均值即可。
+  var image = new Image();
+  image.src = "/src/images/1.jpg";
+
+  image.onload = function () {
+    cxt.drawImage(image, 10, 10);
+    var imgData = cxt.getImageData(10, 10, 120, 120);
+    var data = imgData.data;
+    //遍历每个像素
+    for (var i = 0; i < data.length; i += 4) {
+      var grayscale = data[i] * 0.3 + data[i + 1] * 0.6 + data[i + 2] * 0.1;
+      data[i + 0] = grayscale;　 //红
+      data[i + 1] = grayscale;　 //绿
+      data[i + 2] = grayscale;　 //蓝
+    }
+    //在指定位置输出图片
+    cxt.putImageData(imgData, 10, 10);
+  }
+}());
