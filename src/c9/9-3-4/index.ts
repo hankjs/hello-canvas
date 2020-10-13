c74b97aec3047cd31aa6e1f41385d4fc18cac2b3
zjhcn <332124614@qq.com>
2020-10-13 17:29:33 +0800

    feat(c9 - 渐变与阴影):
    - 阴影用于图片，4方向阴影

---
+++ src/c9/9-3-4/index.ts
@@ -0,0 +1,21 @@
+import { dom } from "@/utils/dom";
+
+const cnv = dom("canvas") as HTMLCanvasElement;
+const cxt = cnv.getContext("2d") as CanvasRenderingContext2D;
+
+; (async function () {
+  //创建image对象
+  var image = new Image();
+  image.src = "/src/images/1.jpg";
+
+  image.onload = function () {
+    //定义阴影
+    cxt.shadowOffsetX = 0;
+    cxt.shadowOffsetY = 0;
+    cxt.shadowColor = "HotPink";
+    cxt.shadowBlur = 10;
+    cxt.fillRect(40, 15, 120, 120);
+
+    cxt.drawImage(image, 40, 15, 120, 120);
+  }
+}());
