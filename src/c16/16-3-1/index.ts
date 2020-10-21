5146a0deea9c2f104ad534700c4b1bc8d838d126
zjhcn <332124614@qq.com>
2020-10-21 15:14:09 +0800

    feat(c16 - 碰撞检测): 外接圆判定法
    - 两圆测试

--- src/c16/16-3-1/index.ts
+++ src/c16/16-3-1/index.ts
@@ -1,40 +1,31 @@
 import { Ball } from "@/objects/Ball";
 import { animationFrame, dom, getKey, getMouse } from "@/utils/dom";
-import { checkRect } from "@/utils/tools";
+import { checkCircle, checkRect } from "@/utils/tools";
 
 const cnv = dom("canvas") as HTMLCanvasElement;
 const cxt = cnv.getContext("2d") as CanvasRenderingContext2D;
 const p = dom("p") as HTMLParagraphElement;
 
 ; (async function () {
-  //定义一个位置固定的小球ballA
-  var ballA = new Ball(cnv.width / 2, cnv.height / 2, 30);
-  //获取ballA的外接矩形
-  var rectA = ballA.getRect();
+  //定义一个位置固定的小球
+  var ballA = new Ball(cnv.width / 2, cnv.height / 2, 20, "#FF6699");
   var mouse = getMouse(cnv);
 
   (function frame() {
     animationFrame(frame);
     cxt.clearRect(0, 0, cnv.width, cnv.height);
 
-    //绘制ballA以及它的外接矩形
-    ballA.fill(cxt);
-    cxt.strokeRect(rectA.x, rectA.y, rectA.width, rectA.height);
-
-    //定义一个位置不固定的小球ballB，小球追随鼠标
-    var ballB = new Ball(mouse.x, mouse.y, 30);
-    //获取ballB的外接矩形
-    var rectB = ballB.getRect();
-
-    //绘制ballB以及它的外接矩形
-    ballB.fill(cxt);
-    cxt.strokeRect(rectB.x, rectB.y, rectB.width, rectB.height);
+    //定义一个位置不固定的小球，小球追随鼠标
+    var ballB = new Ball(mouse.x, mouse.y, 20, "#66CCFF");
 
     //碰撞检测
-    if (checkRect(rectA, rectB)) {
+    if (checkCircle(ballB, ballA)) {
       p.innerHTML = "撞上了";
     } else {
       p.innerHTML = "没撞上";
     }
+
+    ballA.fill(cxt);
+    ballB.fill(cxt);
   })();
 }());
